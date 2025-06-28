dbaa33a6809e7ce63f2b0572e1509033beacf08e
Harrisijaz <harisijaz278@gmail.com>
2025-06-28 12:58:36 -0700

    ErrorComponent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./Components/Landing/Landing";
 import Dashboard from "./Components/Landing/Dashboard";
 import Oauthcallback from "./Components/Landing/Oauthcallback";
+import ErrorComponent from "./Components/Landing/ErrorComponent";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/dash" element={<Dashboard />} />
          <Route path="/oauth/callback" element={<Oauthcallback />} />
+         <Route path="/*" element={<ErrorComponent />} />
       </Routes>
     </Router>
   );
---
+++ src/Components/Landing/ErrorComponent.jsx
@@ -0,0 +1,12 @@
+export default function ErrorComponent(){
+
+    return (
+        <div className="container">
+
+            <h1>We are working Really Hard on this page 😊</h1>
+            <div>
+                <h3>Thanks 🥰</h3>
+            </div>
+        </div>
+    )
+}
