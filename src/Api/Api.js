2c51d1912a990834f401d7d14f86ffef36aab494
Kashif Ali <kashifali0969082@gmail.com>
2025-07-18 04:59:41 +0500

    Fix CORS Network Error with Proxy Configuration

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://api.adaptivelearnai.xyz';
+const BASE_URL = '';
 
 
 const invoke = ({ url, method = 'GET', headers, data, ...rest }) => axios({ baseURL: BASE_URL, url, method, headers, data, ...rest });
