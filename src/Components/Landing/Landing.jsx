6c1a468ea8e1fe5eed3029ad369ccd2a7dd28297
Abdullah Naseem <abdullahnaseem27@gmail.com>
2025-07-25 07:09:05 +0500

    update Landing.jsx

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -338,7 +338,7 @@ import "./Landing";
               <div className="ai-logo">
                 <img 
                   src="/book.svg" 
-                  alt="book.svg"
+                  alt="book"
                   className="adaptive-learn-logo w-64 h-64 md:w-72 md:h-72 lg:w-80 lg:h-80"
                 />
               </div>
